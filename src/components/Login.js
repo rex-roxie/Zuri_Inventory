bdc6fca66efb2837561a40ca78b0edbc253f36e5
rexroxie <rex.roxie@outlook.com>
2023-02-14 19:05:51 +0300

    Revert "Basic auth"
    This reverts commit 32accce252eee25b92d19f5fe537131cc1807ce7.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,8 @@
-import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-
-  const login = (event) => {
-    event.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      navigate('/home');
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(`Error Code: ${errorCode} \n Error message: ${errorMessage}`);
-    });
-  }
-
   return (
-    <div>
-      <form>
-        <input type="email" placeholder='Email' onChange={(event) => setEmail(event.target.value)}/>
-        <input type="password" placeholder='Password' onChange={(event) => setPassword(event.target.value)} />
-        <button onClick={login}>Login</button>
-      </form>
-    </div>
+    <div>Login</div>
   )
 }
 
