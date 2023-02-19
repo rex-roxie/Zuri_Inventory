39011df487f5805b70fb3910000f967e9f49a462
rexroxie <rex.roxie@outlook.com>
2023-02-19 18:25:24 +0300

    Messed up big time

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,4 +18,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+const auth = getAuth(app);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+export { 
+  auth,
+  db
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { auth } from '../firebase';
+import React, { useEffect, useState } from 'react';
+import { auth, db } from '../firebase';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
 
 const Home = () => {
+  const [drivers, setDrivers] = useState([]);
   const navigate = useNavigate();
 
   const logout = () => {
@@ -11,9 +13,23 @@ const Home = () => {
     navigate('/');
   }
 
+  const colRef = collection(db, "drivers");
+  getDocs(colRef)
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        setDrivers({ ...doc.data(), id: doc.id})
+      })
+      console.log(drivers)
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+    
+
   return (
     <div>
         <h1>Welcome Home {auth.currentUser.email}</h1>
+        {drivers.phone}
         <button onClick={logout}>Logout</button>
     </div>
   )
