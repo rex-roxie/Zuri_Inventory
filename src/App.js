4f7fd0a06b20e6d42eb8dcd2a48a66dd786f3ebf
rexroxie <rex.roxie@outlook.com>
2023-02-19 14:45:55 +0300

    Finish auth

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import { Routes, Route } from 'react-router';
 import Login from './components/Login';
 import Home from './pages/Home';
+import { AuthProvider } from './components/AuthChange';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path={"/"} element={<Login />} />
-        <Route path={"/home"} element={<Home />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path={"/"} element={<Login />} />
+          <Route path={"/home"} element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        </Routes>
+        <AuthProvider />
+      </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { auth } from '../firebase';
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    signOut(auth);
+    navigate('/');
+  }
+
   return (
     <div>
-        <h1>Welcome Home</h1>
+        <h1>Welcome Home {auth.currentUser.email}</h1>
+        <button onClick={logout}>Logout</button>
     </div>
   )
 }
