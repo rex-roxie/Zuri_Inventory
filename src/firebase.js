db00b227616e6c406220b1421464431e51d465a4
rexroxie <rex.roxie@outlook.com>
2023-04-08 19:08:02 +0300

    change firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-vATTTIFWfA402thdHn-1Yv_NjnTZjQE",
-  authDomain: "zuri-inv.firebaseapp.com",
-  projectId: "zuri-inv",
-  storageBucket: "zuri-inv.appspot.com",
-  messagingSenderId: "430789854380",
-  appId: "1:430789854380:web:eb3a3af65c961d8d0e8b09"
+  apiKey: "AIzaSyDG3jU1svpiybKykY8HnJ3kZnL8J-pT_MM",
+  authDomain: "zuri-erp.firebaseapp.com",
+  projectId: "zuri-erp",
+  storageBucket: "zuri-erp.appspot.com",
+  messagingSenderId: "682392835677",
+  appId: "1:682392835677:web:e3d8deedc2557fef071fb4"
 };
 
 // Initialize Firebase
